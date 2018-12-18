5431c630509905d1ef098daa18f701766a5f517a
Othman Mejdoubi <o.mejdoubi@gmail.com>
2018-12-18 06:55:27 +0100

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'wafaa',
+    text: 'hey othman'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(msg) {
+  console.log('New message', msg);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'othman',
+    text: 'hello there',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (msg) => {
+    console.log('Create message', msg);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
