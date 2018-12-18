1dbf7749919eb617c881e538cb7819475fc49026
Othman Mejdoubi <o.mejdoubi@gmail.com>
2018-12-18 11:53:26 +0100

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,12 @@ socket.on('newLocationMessage', function(msg) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function() {
-
+    messageTextBox.val('');
   });
 });
 
@@ -43,12 +44,16 @@ locationButton.on('click', function(e) {
     return alert('Geolocation not supported by your browser.')
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
   navigator.geolocation.getCurrentPosition(function(position) {
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
+    locationButton.removeAttr('disabled').text('Send location');
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
